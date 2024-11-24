b2fd4afb4cf6cda44d93e77283651ff9d936b029
Andeskjerf <predgd@pm.me>
2024-11-24 17:23:57 +0100

    feat: write tests for cache

--- src/services/cache.rs
+++ src/services/cache.rs
@@ -68,8 +68,138 @@ fn cache_dir() -> Result<PathBuf, Box<dyn Error>> {
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::consts::{LONG_BREAK_TIME, SHORT_BREAK_TIME, WORK_TIME};
 
     use super::*;
-    use std::fs;
+
+    // Mock the env variables used in the code
+    const MODULE: &str = "waybar-module-pomodoro";
+    const VERSION: &str = "0.1";
+
+    fn create_timer(
+        work_time: Option<u16>,
+        short_break: Option<u16>,
+        long_break: Option<u16>,
+    ) -> Timer {
+        Timer {
+            current_index: 1,
+            elapsed_millis: 950,
+            elapsed_time: 300,
+            times: [
+                work_time.unwrap_or(25),
+                short_break.unwrap_or(5),
+                long_break.unwrap_or(15),
+            ],
+            iterations: 2,
+            session_completed: 8,
+            running: false,
+            socket_nr: 0,
+        }
+    }
+
+    #[test]
+    fn test_store_and_restore() -> Result<(), Box<dyn Error>> {
+        unsafe {
+            std::env::set_var("CARGO_PKG_NAME", MODULE);
+            std::env::set_var("CARGO_PKG_VERSION", VERSION);
+        }
+
+        let timer = create_timer(None, None, None);
+        store(&timer)?;
+        let mut restored_timer = create_timer(Some(30), Some(10), Some(20));
+
+        let config = Config {
+            work_time: 25,
+            short_break: 5,
+            long_break: 15,
+            ..Default::default()
+        };
+
+        restore(&mut restored_timer, &config)?;
+
+        assert_eq!(restored_timer.current_index, timer.current_index);
+        assert_eq!(restored_timer.elapsed_millis, timer.elapsed_millis);
+        assert_eq!(restored_timer.elapsed_time, timer.elapsed_time);
+        assert_eq!(restored_timer.times, timer.times);
+        assert_eq!(restored_timer.iterations, timer.iterations);
+        assert_eq!(restored_timer.session_completed, timer.session_completed);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_store_and_restore_mismatched_config() -> Result<(), Box<dyn Error>> {
+        unsafe {
+            std::env::set_var("CARGO_PKG_NAME", MODULE);
+            std::env::set_var("CARGO_PKG_VERSION", VERSION);
+        }
+
+        let timer = create_timer(None, None, None);
+        store(&timer)?;
+        let mut restored_timer = create_timer(Some(30), Some(10), Some(20));
+
+        let config = Config {
+            work_time: 30,
+            short_break: 10,
+            long_break: 20,
+            ..Default::default()
+        };
+
+        restore(&mut restored_timer, &config)?;
+
+        // Check if the restored timer state is not changed
+        assert_eq!(restored_timer.current_index, 1);
+        assert_eq!(restored_timer.elapsed_millis, 950);
+        assert_eq!(restored_timer.elapsed_time, 300);
+        assert_eq!(restored_timer.times, [30, 10, 20]);
+        assert_eq!(restored_timer.iterations, 2);
+        assert_eq!(restored_timer.session_completed, 8);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_cache_dir_creation() -> Result<(), Box<dyn Error>> {
+        unsafe {
+            std::env::set_var("CARGO_PKG_NAME", MODULE);
+            std::env::set_var("CARGO_PKG_VERSION", VERSION);
+        }
+
+        let mut dir = dirs::cache_dir().expect("unable to get cache dir");
+        dir.push(MODULE);
+
+        let result = cache_dir()?;
+
+        assert_eq!(result, dir);
+        assert!(dir.is_dir());
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_match_timers_match() {
+        let config = Config {
+            work_time: 25,
+            short_break: 5,
+            long_break: 15,
+            ..Default::default()
+        };
+
+        let times = [25, 5, 15];
+
+        assert!(match_timers(&config, &times));
+    }
+
+    #[test]
+    fn test_match_timers_mismatch() {
+        let config = Config {
+            work_time: 30,
+            short_break: 10,
+            long_break: 20,
+            ..Default::default()
+        };
+
+        let times = [25, 5, 15];
+
+        assert!(!match_timers(&config, &times));
+    }
 }
