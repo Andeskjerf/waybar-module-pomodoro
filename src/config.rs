c9a5603283179492615590b336b9e7c0db739733
Khushal <khushal.banks@gmail.com>
2024-11-24 09:27:52 +0000

    Updated config.rs to replace autor variable with persist

--- src/config.rs
+++ src/config.rs
@@ -15,7 +15,7 @@ pub struct Config {
     pub break_icon: String,
     pub autow: bool,
     pub autob: bool,
-    pub autor: bool,
+    pub persist: bool,
     pub binary_name: String,
 }
 
@@ -34,7 +34,7 @@ impl Config {
         let mut break_icon = BREAK_ICON.to_string();
         let mut autow = false;
         let mut autob = false;
-        let mut autor = false;
+        let mut persist = false;
 
         let binary_path = options.first().unwrap();
         let binary_name = binary_path.split('/').last().unwrap().to_string();
@@ -68,7 +68,7 @@ impl Config {
             }
             "--autow" => autow = true,
             "--autob" => autob = true,
-            "--autor" => autor = true,
+            "--persist" => autor = true,
             "--no-icons" => no_icons = true,
             "--no-work-icons" => no_work_icons = true,
             _ => (),
@@ -86,7 +86,7 @@ impl Config {
             break_icon,
             autow,
             autob,
-            autor,
+            persist,
             binary_name,
         }
     }
